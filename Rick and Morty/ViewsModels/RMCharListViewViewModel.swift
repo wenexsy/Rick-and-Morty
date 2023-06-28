b3c34c015a3bafffe75ad9ca9aa3d7cabbec8323
Wenexsy <wenexsy@mail.ru>
2023-06-28 14:59:01 +0300

    added char cell

--- Rick and Morty/ViewsModels/RMCharListViewViewModel.swift
+++ Rick and Morty/ViewsModels/RMCharListViewViewModel.swift
@@ -7,28 +7,38 @@
 
 import UIKit
 
-final class CharListViewViewModel: NSObject {
+final class RMCharListViewViewModel: NSObject {
     func fetchCharacters() {
         RMServise.shared.execute(.listCharRequest, expecting: RMGetCharsResponse.self) { result in
             switch result {
             case .success(let model):
-                print("Total: "+String(model.info.count))
+                print("Example image url: "+String(model.results.first?.image ?? "No image"))
                 print("Page result count "+String(model.results.count))
-
+                
             case .failure(let error):
                 print(String(describing: error))
             }
         }
     }
 }
-extension CharListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+extension RMCharListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemRed
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier,
+            for: indexPath
+        ) as? RMCharacterCollectionViewCell else {
+            fatalError("Unsupported cell")
+        }
+        let viewModel = RMCharacterCollectionViewCellViewModel(
+            characterName: "Afraz",
+            characterStatus: .alive,
+            characterImageUrl:URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg")
+        )
+        cell.configure(with: viewModel)
         return cell
     }
     
